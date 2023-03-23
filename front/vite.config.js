8c14f583b23ec8816b7a50f2f40dca73588a90e2
RayuRuna <quentin.bisiaux@gmail.com>
2023-03-23 17:32:05 +0100

    init vue is finally working

--- front/vite.config.js
+++ front/vite.config.js
@@ -6,10 +6,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  root: './public',
+  root: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  optimizeDeps: {
+    include: [],
+    exclude: []
   }
 })
